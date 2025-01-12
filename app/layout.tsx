f73a01e9dda24b7bf3740b8926b2640dae6e0802
Vector <vector9527@proton.me>
2025-01-12 17:57:24 +0800

    Configure QueryClient with default refetch interval for improved data freshness in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,13 @@ import { WagmiProvider } from "wagmi";
 import { config } from "./config/wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchInterval: 5_000, // 5s 刷新一次
+    },
+  },
+});
 
 export default function RootLayout({
   children,
